27a1a5da573eee8c1882f7a334761272348ee3a8
yunjin Choi <89494907+yunjinchoidev@users.noreply.github.com>
2022-10-08 09:42:36 +0900

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 5001
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://root:1111@yunjinchoi.ltc9y3z.mongodb.net/test',{
+}).then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World! gg')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
